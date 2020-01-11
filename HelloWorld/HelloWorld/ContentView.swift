29aa399ddb7fc9f58ebbadac9298ffea32b1b9a3
Nogami111 <59758384+Nogami111@users.noreply.github.com>
2020-01-11 15:50:45 +0900

    initial commit

---
+++ HelloWorld/HelloWorld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorld
+//
+//  Created by 市川大樹 on 2020/01/11.
+//  Copyright © 2020 Nogami. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
